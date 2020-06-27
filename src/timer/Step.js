8ecb6c416aa09421f00b9bc2bfb0329dfec0633e
Joe Knowles <joejknowles@gmail.com>
2020-06-27 14:46:24 +0100

    Add animation

--- src/timer/Step.js
+++ src/timer/Step.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const Root = styled.div`
@@ -7,8 +7,29 @@ const Root = styled.div`
   padding-top: 10vh;
   height: 90vh;
   background-color: hsl(240, 10%, 90%);
+  position: relative;
+`;
+
+const ProgressBar = styled.div`
+  position: absolute;
+  top: ${({hasStarted}) => hasStarted ? 0 : "100%"};
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: hsla(240, 10%, 0%, 0.15);
+  ${({hasStarted}) => hasStarted ? "transition: top 3.9s linear" : ""};
 `;
 
 export const Step = ({ name }) => {
-  return <Root>{name}</Root>;
+  const [hasStarted, setHasStarted] = useState(false);
+  useEffect(() => {
+    setHasStarted(false);
+    setTimeout(() => setHasStarted(true), 100);
+  }, [name])
+  return (
+    <Root>
+      {name}
+      <ProgressBar {...{hasStarted}}/>
+    </Root>
+  );
 };
