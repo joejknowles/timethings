8d0932af316c4608555c824adb7461cc04c0d05a
Joe Knowles <joejknowles@gmail.com>
2020-06-27 14:55:16 +0100

    Add hash and url timer

--- src/timer/startTimer.js
+++ src/timer/startTimer.js
@@ -1,6 +1,7 @@
 import { incrementTo } from "../utils";
 
 export const startTimer = (setIndex, startIndex, steps) => {
+  startHashTimer();
   const nextStep = (index) => {
     const step = steps[index];
     setTimeout(() => {
@@ -10,4 +11,15 @@ export const startTimer = (setIndex, startIndex, steps) => {
     }, step.duration);
   };
   nextStep(startIndex);
+};
+
+const startHashTimer = () => {
+  const nextSecond = (seconds) => {
+    window.history.pushState(null, null, `#${seconds}`);
+    document.title = "breath " + seconds;
+    setTimeout(() => {
+      nextSecond(seconds + 1);
+    }, 1000);
+  };
+  nextSecond(0);
 };
