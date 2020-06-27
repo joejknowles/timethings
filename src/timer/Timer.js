1a91a8a677b7a429e6d4c4f4b9e109afbf7db19e
Joe Knowles <joejknowles@gmail.com>
2020-06-27 13:13:29 +0100

    Delete unused things

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { boxBreathing } from "./presetTimers";
-import { incrementTo } from "../utils";
 import { Step } from "./Step";
 import { startTimer } from "./startTimer";
 
