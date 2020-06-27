c6cd007e595a245eb8726113a0a3e737a76ee5de
Joe Knowles <joejknowles@gmail.com>
2020-06-27 10:56:22 +0100

    Make test work slowly

--- cypress/integration/examples/timer.spec.js
+++ cypress/integration/examples/timer.spec.js
@@ -2,17 +2,16 @@
 
 context('Timer', () => {
   beforeEach(() => {
-    cy.clock();
     cy.visit('/')
   })
 
   it('Displays Box Breathing steps correctly', () => {
     cy.contains("Breathe in");
-    cy.tick(4000);
+    cy.wait(4000)
     cy.contains("Hold");
-    cy.tick(4000);
+    cy.wait(4000)
     cy.contains("Breathe out");
-    cy.tick(4000);
+    cy.wait(4000)
     cy.contains("Hold");
   })
 })
