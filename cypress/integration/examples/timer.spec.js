07780e302de7d81df33f7aff37a023f35fffc4f7
Joe Knowles <joejknowles@gmail.com>
2020-06-27 10:41:53 +0100

    Test Timer

---
+++ cypress/integration/examples/timer.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+context('Timer', () => {
+  beforeEach(() => {
+    cy.clock();
+    cy.visit('/')
+  })
+
+  it('Displays Box Breathing steps correctly', () => {
+    cy.contains("Breathe in");
+    cy.tick(4000);
+    cy.contains("Hold");
+    cy.tick(4000);
+    cy.contains("Breathe out");
+    cy.tick(4000);
+    cy.contains("Hold");
+  })
+})
