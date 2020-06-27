58a28f9dc7a0bbba0bf0ff531c8c249af62f4ee5
Joe Knowles <joejknowles@gmail.com>
2020-06-27 13:12:01 +0100

    Refactor startTimer

--- src/timer/startTimer.js
+++ src/timer/startTimer.js
@@ -1,10 +1,13 @@
 import { incrementTo } from "../utils";
 
-export const startTimer = (setIndex, index, steps) => {
-  const step = steps[index];
-  setTimeout(() => {
-    const nextIndex = incrementTo(index, 3);
-    setIndex(nextIndex);
-    startTimer(setIndex, nextIndex, steps);
-  }, step.duration);
+export const startTimer = (setIndex, startIndex, steps) => {
+  const nextStep = (index) => {
+    const step = steps[index];
+    setTimeout(() => {
+      const nextIndex = incrementTo(index, 3);
+      setIndex(nextIndex);
+      nextStep(nextIndex);
+    }, step.duration);
+  };
+  nextStep(startIndex);
 };
